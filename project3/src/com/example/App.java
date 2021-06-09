4a53b458a8d46917fd8fa1604ffc1c774abaa1d5
001ZQ9744 <001ZQ9744@LAPTOP-JAT4IUDE>
2021-06-09 14:50:02 +0530

    added commit

--- project3/src/com/example/App.java
+++ project3/src/com/example/App.java
@@ -5,14 +5,22 @@ public class App {
 	public static void main(String args[])
 	{
 		AccountClass account=new AccountClass();
-		account.createAccount("acc-1", 200.0, "John", AccountType.SAVINGS);
-		if(account.getAccountType()==AccountType.SAVINGS)
+		account.createAccount("acc-1", 200.0, "John", AccountType.CURRENT);
+		
+		if(account.getAccountType()==AccountType.CURRENT)
 		{
-			System.out.println("savings account. . ");
-		}
-		else
-		{
-			System.out.println("current account");
+			if(account.getAccountType()==AccountType.CURRENT)
+			{
+				CurrentAccount cur=new CurrentAccount();
+				cur.createCurrentAccount();
+				
+			}
+			else if(account.getAccountType()==AccountType.SAVINGS)
+			{
+				SavingsAccount cur=new SavingsAccount();
+				cur.createSavingsAccount();
+				
+			}
 		}
 	}
 
