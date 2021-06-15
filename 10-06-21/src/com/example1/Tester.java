e8359973aa0e92e91e72699e6b79ecde6555ecd5
001ZQ9744 <001ZQ9744@LAPTOP-JAT4IUDE>
2021-06-15 11:56:34 +0530

    I/O StreamsDemo

--- 10-06-21/src/com/example1/Tester.java
+++ 10-06-21/src/com/example1/Tester.java
@@ -1,11 +1,19 @@
 package com.example1;
 
+import java.util.Scanner;
+
 public class Tester {
+	private static Scanner scanner = new Scanner(System.in);
+
 
 	public static void main(String[] args) {
 		
-		Vehicle myVehicle=new Truck();
-		System.out.println(myVehicle.calcTripDistance(100.9, 900.8));
+		Tester tester = new Tester();
+		
+		
+		
+		/*Vehicle myVehicle=new Truck();
+		/*System.out.println(myVehicle.calcTripDistance(100.9, 900.8));
 		System.out.println(myVehicle.calcFuelEfficiency(45));
 		System.out.println(myVehicle.getDetails());
 		myVehicle=new RiverBarge();
@@ -13,8 +21,29 @@ public class Tester {
 		System.out.println(myVehicle.calcFuelEfficiency(1000));
 		System.out.println(myVehicle.getDetails());
 		myVehicle.setMessage("I Am On Riverbarge");
-		System.out.println(myVehicle.getMessage());
+		System.out.println(myVehicle.getMessage());*/
+		
+		SeaPlane seaPlane = null;
+		RiverBarge riverBarge = new RiverBarge();
+		
+		tester.check(seaPlane);
+		
+		private void check(SeaPlane o) {
+		try {
+		if (o instanceof SeaPlane) {
+			SeaPlane seaPlane = (SeaPlane) o;
+			System.out.println("Seaplane");
+		}
+			
+			
+			
+		}
+		catch (NullPointerException e) {
+	     System.err.println("null pointer exception");
+		 }
+		
+	}	
+		
 
-	}
 
 }
