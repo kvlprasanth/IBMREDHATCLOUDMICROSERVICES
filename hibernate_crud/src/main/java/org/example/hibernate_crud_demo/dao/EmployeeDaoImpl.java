8f75cd12f3c498dbe85190714ed8ec689fd8b267
001ZQ9744 <001ZQ9744@LAPTOP-JAT4IUDE>
2021-06-22 21:07:45 +0530

    hibernate crud

---
+++ hibernate_crud/src/main/java/org/example/hibernate_crud_demo/dao/EmployeeDaoImpl.java
@@ -0,0 +1,100 @@
+package org.example.hibernate_crud_demo.dao;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+import javax.persistence.Query;
+
+import org.example.hibernate_crud_demo.factory.MySessionFactory;
+import org.example.hibernate_crud_demo.model.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+
+
+public class EmployeeDaoImpl implements EmployeeDao {
+	
+	private MySessionFactory mySessionFactory;
+	private SessionFactory sessionFactory;
+	private Session session;
+	private static Scanner scanner=new Scanner(System.in);
+	
+	{
+		mySessionFactory=MySessionFactory.createMySessionFactory();
+		sessionFactory=mySessionFactory.getSessionFactory();
+		session=sessionFactory.openSession();
+	}
+
+	@Override
+	public Employee createEmployee(Employee employee) {
+		session=sessionFactory.openSession();
+		session.getTransaction().begin();
+		session.save(employee);
+		session.getTransaction().commit();
+		return employee;
+	}
+
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Employee> displayAllEmployee() {
+		session=sessionFactory.openSession();
+	
+		Query query=session.createQuery("select E from Employee E");
+		return query.getResultList();
+		
+	}
+
+	@Override
+	public Employee getemployeeById(Integer id) {
+		session=sessionFactory.openSession();
+		return session.get(Employee.class, id);
+	}
+
+	@Override
+	public Employee updateEmployee(Integer id) {
+		session=sessionFactory.openSession();
+		Employee tempEmployee= session.get(Employee.class, id);
+		if(tempEmployee ==null)
+		{
+			
+			throw new EmployeeNotFoundException("employee not found.");
+			
+		}
+		System.out.print("First Name: ");
+		String firstName=scanner.next();
+		System.out.print("Last Name: ");
+		String lastName=scanner.next();
+		System.out.print("Email: ");
+		String email=scanner.next();
+		tempEmployee.setFirstName(firstName);
+		tempEmployee.setLastName(lastName);
+		tempEmployee.setEmail(email);
+		session.getTransaction().begin();
+		//merge for update an existing entity
+		session.merge(tempEmployee);
+		session.getTransaction().commit();
+		
+		return tempEmployee;
+		
+		
+	}
+
+	@Override
+	public Employee deleteEmployee(Integer id) {
+		session=sessionFactory.openSession();
+		Employee tempEmployee= session.get(Employee.class, id);
+		if(tempEmployee ==null)
+		{
+			
+			throw new EmployeeNotFoundException("employee not found.");
+			
+		}
+		session.getTransaction().begin();
+		session.remove(tempEmployee);
+		session.getTransaction().commit();
+		return tempEmployee;
+	}
+
+}
